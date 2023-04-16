41b7e76ba28c2f0866b6c3216adc1f8408c93e45
emilvn <emilvnielsen@hotmail.com>
2023-04-16 14:51:30 +0200

    changed from async await to then catch

--- main.js
+++ main.js
@@ -4,91 +4,84 @@ import { displayAllItems } from "./modules/displayFuncs.js";
 
 window.addEventListener("load", main);
 
-async function main() {
+function main() {
     const DataURL = "test.json";
-    try {
-        const items = await getData(DataURL);
-        displayAllItems(items); 
-    }
-    catch (err) {
-        throw new Error(`Error at main: ${err}`);
-    }
+    getData(DataURL)
+        .then(response => displayAllItems(response))
+        .catch(err => { throw new Error(`Error at main: ${err}`) });
 }
 
 /* ========== CREATE ========== */
-async function addData(dbURL, data) {
-    try {
-        const response = await fetch(dbURL, {
+function addData(dbURL, data) {
+    fetch(dbURL, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (response.ok) {
+                console.log("Data added successfully!");
+            }else {
+                throw new Error("Bad response at addData.");
+            }
         })
-        if (response.ok) {
-            console.log("Data added successfully!");
-        }
-        else {
-            throw new Error("Bad response at addData.");
-        }
-    }
-    catch (err) {
-        throw new Error(`Error at addData: ${err}`);
-    }
+        .catch(err => {
+            throw new Error(`Error at addData: ${err}`)
+        });
 }
-/* ========== READ ========== */
-async function getData(dbURL) {
-    try {
-        const response = await fetch(dbURL);
-        if (response.ok) {
-            console.log("Data retrieved successfully!");
-            return await response.json();
-        }
-        else {
-            throw new Error("Bad response at getData.");
-        }
-    }
-    catch (err) {
-        throw new Error(`Error at getData: ${err}`);
-    }
+function getData(dbURL) {
+    return fetch(dbURL)
+        .then(response => {
+            if (response.ok) {
+                console.log("Data retrieved successfully!");
+                return response.json();
+            } else {
+                throw new Error("Bad response at getData.");
+            }
+        })
+        .catch(err => {
+            throw new Error(`Error at getData ${err}`);
+        });
 }
 /* ========== UPDATE ========== */
-async function updateData(dbURL, updatedData) {
-    try {
-        const response = await fetch(dbURL +"/"+ updatedData.id, {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                id: updatedData.id
-            })
+function updateData(dbURL, updatedData) {
+    fetch(dbURL + "/" + updatedData.id, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            id: updatedData.id
         })
-        if (response.ok) {
-            console.log("Data updated successfully!");
-        }
-        else {
-            throw new Error("Bad response at updateData");
-        }
-    }
-    catch (err) {
-        throw new Error(`Error at updateData: ${err}`);
-    }
+    })
+        .then(response => {
+            if (response.ok) {
+                console.log("Data updated successfully!");
+            }
+            else {
+                throw new Error("Bad response at updateData");
+            }
+        })
+        .catch(err => {
+            throw new Error(`Error at updateData: ${err}`);
+        });
 }
 /* ========== DELETE ========== */
-async function deleteData(dbURL, dataID) {
-    try {
-        const response = await fetch(dbURL + "/" + dataID, {
-            method: "DELETE",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                id: dataID
-            })
+function deleteData(dbURL, dataID) {
+    fetch(dbURL + "/" + dataID, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            id: dataID
         })
-        if (response.ok) {
-            console.log("Data deleted successfully!");
-        }
-        else {
-            throw new Error("Bad response at deleteData");
-        }
-    }
-    catch (err) {
-        throw new Error(`Error at deleteData: ${err}`);
-    }
+    })
+        .then(response => {
+            if (response.ok) {
+                console.log("Data deleted successfully!");
+            }
+            else {
+                throw new Error("Bad response at deleteData");
+            }
+        })
+        .catch(err => {
+            throw new Error(`Error at deleteData: ${err}`);
+        });
 }
